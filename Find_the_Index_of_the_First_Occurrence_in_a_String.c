e6188f1019db675f43c78f838309228bb8a8d9e8
jbelkerf <jbelkerf@gmail.com>
2025-02-02 15:25:08 +0100

    Revert "fixing some norminitte erros and protecting agains SEGV that may caused by derefrencing a NULL pointer"
    This reverts commit 7aeebfa239ae9d6f387ec8d5a70deda1ac2a4a98.

--- Find_the_Index_of_the_First_Occurrence_in_a_String.c
+++ Find_the_Index_of_the_First_Occurrence_in_a_String.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
+int strStr(char* haystack, char* needle) {
+    int i;
+    int j;
+    int k = 0;
 
-int	strStr(char *haystack, char *needle)
-{
-	int	i;
-	int	j;
-	int	flag;
-	int	k;
-
-	i = 0;
-	k = 0;
-	flag = -1;
-	if (!haystack || !needle)
-		return (-1);
-	while (haystack[i])
-	{
-		j = 0;
-		while (needle[j] == haystack[i + j])
-		{
-			j++;
-			if (needle[j] == '\0')
-				return (i);
-		}
-		i++;
-	}
-	return (-1);
+    i = 0;
+    j = 0;
+    int flag = -1;
+    while(haystack[i])
+    {
+        j = 0;
+            while(needle[j] == haystack[i + j])
+            {
+                j++;
+                if(needle[j] == '\0')
+                    return i;
+            }
+        i++;
+    }
+        return -1;
 }
